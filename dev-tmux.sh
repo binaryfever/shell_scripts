1fdb9808d91cd6ab2fe2df77aa6c13bd00c0867f
Fred McHale <binaryfever@gmail.com>
2020-05-27 17:23:02 -0700

    Updated to start vim and npm start

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-cd ~/Documents/Projects
-tmux new-session -s 'dev' \; split-window -h \; split-window -v \; resize-pane -R 40 \; attach
+
+#Opens a development environment that I like in tmux at the given directory
+#main pane opens in vim (using nerd tree
+#second pane launches with npm start 
+#third pane launches 
+if [ "$1" != "" ]; then
+  cd $1 
+  tmux new-session -s 'dev' 'vim' \; split-window -h 'npm start' \; split-window -v \; resize-pane -R 40 \; attach
+else
+  echo "Please enter a directory"
+fi
+
